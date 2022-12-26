492dfd17069a2ec8caf8f8bf655ac1fb3ce7bb41
Dominik "Dodo" Dopplinger <dom.dopplinger@gmail.com>
2022-12-26 19:04:45 +0100

    Solution day 3

---
+++ src/solutions/day_03.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from src.util.file_util import read_input_file
+
+
+def is_triangle_possible(a, b, c) -> bool:
+    return a + b > c and a + c > b and b + c > a
+
+
+def line_to_list(line: str) -> List[int]:
+    return list(map(int, filter(lambda p: p != "", line.split(" "))))
+
+
+def lines_to_triangles(lines: List[str]) -> List[List[int]]:
+    return list(map(line_to_list, lines))
+
+
+def columns_to_triangles(lines: List[str]) -> List[List[int]]:
+    triangles = []
+    for i in range(0, len(lines), 3):
+        line_1 = line_to_list(lines[i])
+        line_2 = line_to_list(lines[i + 1])
+        line_3 = line_to_list(lines[i + 2])
+        for j in range(3):
+            triangles.append([line_1[j], line_2[j], line_3[j]])
+    return triangles
+
+
+def day_03_1() -> int:
+    lines = read_input_file(3)
+    return len(list(filter(lambda t: is_triangle_possible(t[0], t[1], t[2]), lines_to_triangles(lines))))
+
+
+def day_03_2() -> int:
+    lines = read_input_file(3)
+    return len(list(filter(lambda t: is_triangle_possible(t[0], t[1], t[2]), columns_to_triangles(lines))))
+
+
+if __name__ == "__main__":
+    print(f"Possible triangles (1): {day_03_1()}")
+    print(f"Possible triangles (2): {day_03_2()}")
